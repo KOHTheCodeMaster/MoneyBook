8085f1b490464b0235d18243058be8a41a51f39c
Harshit Gupta <kohthecodemaster@gmail.com>
2024-01-17 19:00:47 +0530

    2nd Commit - Angular App Components
    Signed-off-by: Harshit Gupta <kohthecodemaster@gmail.com>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import {LeftSidebarComponent} from "./left-sidebar/left-sidebar.component";
+import {MainContentComponent} from "./main-content/main-content.component";
+import {RightSidebarComponent} from "./right-sidebar/right-sidebar.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, LeftSidebarComponent, MainContentComponent, RightSidebarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
